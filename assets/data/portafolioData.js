c21fc333e0b84da0c3d4e90953138af9b88ce68c
Vladimir Abarca <abarca.sant.x@hotmail.com>
2024-10-04 23:58:57 -0500

    first commit

---
+++ assets/data/portafolioData.js
@@ -0,0 +1,37 @@
+export const portafolioData = [
+  {
+    img: "./assets/img/portafolio.png",
+    title: "portafolio",
+    demo: "https://portafolio-nine-kohl.vercel.app/",
+    repo: "https://github.com/Vladimir1292/Portafolio",
+    skills: ["html", "css", "javascript"],
+    content: "Mi primer portafolio. <br> <br>ha sido creado con html, css y javascript"
+  },
+  {
+    img: "./assets/img/encriptador.png",
+    title: "Encriptador de texto",
+    demo: "https://vladimir1292.github.io/Encriptador_de_texto/",
+    repo: "https://github.com/Vladimir1292/Encriptador_de_texto/tree/master",
+    skills: ["html", "css", "javascript"],
+    content: "Encriptador de texto, desarrollado como primer callenge en el programa Alura Latam"
+  },
+  {
+    img: "./assets/img/calculadora.png",
+    title: "Calculadora",
+    demo: "https://calculadora-flax-psi.vercel.app/",
+    repo: "https://github.com/Vladimir1292/calculadora",
+    skills: ["html", "css", "javascript"],
+    content: "Calculadora basica "
+  },
+];
+
+export const skillsData = {
+    html: "devicon:html5",
+    css: "devicon:css3",
+    javascript: "skill-icons:javascript",
+    python: "devicon:python",
+    mysql: "devicon:mysql",
+    java: "devicon:java",
+    trello: "logos:trello",
+    figma: "devicon:figma"
+};
---
+++ assets/data/skillsData.js
@@ -0,0 +1,36 @@
+const skillCard = [
+    {
+        icon: "vscode-icons:file-type-html",
+        title: "html",
+    },
+    {
+        icon: "vscode-icons:file-type-css",
+        title: "CSS",
+    },
+    {
+        icon: "devicon:javascript",
+        title: "Javascript",
+    },
+    {
+        icon: "devicon:python",
+        title: "Python",
+    },
+    {
+        icon: "devicon:mysql",
+        title: "MySQL",
+    },
+    {
+        icon: "devicon:java",
+        title: "Java",
+    },
+    {
+        icon: "logos:trello",
+        title: "Trello",
+    },
+    {
+        icon: "devicon:figma",
+        title: "Figma",
+    },
+]
+
+export default skillCard;
